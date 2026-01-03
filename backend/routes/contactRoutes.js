ba51f6e9fcc08626b0f81405beb4b6c062f7e449
omch-29 <chandwaniom29@gmail.com>
2026-01-03 12:42:42 +0530

    deployed

--- backend/routes/contactRoutes.js
+++ backend/routes/contactRoutes.js
@@ -3,29 +3,10 @@ const Contact = require("../models/Contact");
 const controller = require('../controllers/contactController');
 const router = express.Router();
 
-router.post("/", controller.create
-//    async (req, res) => {
-//   try {
-//     const contact = await Contact.create(req.body);
-//     res.status(201).json(contact);
-//   } catch (error) {
-//     res.status(400).json({ message: error.message });
-//   }
-// }
-);
+router.post("/", controller.create);
 
 
-router.get("/",controller.getContact
-//    async (req, res) => {
-//   const contacts = await Contact.find().sort({ createdAt: -1 });
-//   res.json(contacts);
-// }
-);
-router.delete("/:id",controller.deleteContact
-//    async (req, res) => {
-//   await Contact.findByIdAndDelete(req.params.id);
-//   res.json({ message: "Contact deleted" });
-// }
-);
+router.get("/",controller.getContact);
+router.delete("/:id",controller.deleteContact);
 
 module.exports = router;
